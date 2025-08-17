653f68b5f738437a9c438a70dbdac59ce4b4a67f
Bogdan <bogdanlikuliks@hotmail.com>
2025-08-17 04:57:06 +0200

    Descriptive commit message 13

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,18 +9,24 @@ $( document ).ready(function() {
                     $("#about_scroll").fadeIn();
                     $('#about_left').addClass('animated slideInLeft');
                     $('#about_right').addClass('animated slideInRight');
+                     // Scroll to top
+                    $(window).scrollTop(0);
                     });
                 $("#work").click(function(){
                     $("#index").fadeOut();
                     $("#work_scroll").fadeIn();
                     $('#work_left').addClass('animated slideInLeft');
                     $('#work_right').addClass('animated slideInRight');
+                     // Scroll to top
+                    $(window).scrollTop(0);
                     });
                 $("#contact").click(function(){
                     $("#index").fadeOut();
                     $("#contact_scroll").fadeIn();
                     $('#contact_left').addClass('animated slideInLeft');
                     $('#contact_right').addClass('animated slideInRight');
+                     // Scroll to top
+                    $(window).scrollTop(0);
                     });
                 
                 $(".back").click(function(){
@@ -28,6 +34,8 @@ $( document ).ready(function() {
                     $("#index").fadeIn();
                     $('#index_left').addClass('animated slideInLeft');
                     $('#index_right').addClass('animated slideInRight');
+                     // Scroll to top
+                    $(window).scrollTop(0);
                     });
            
 		});
